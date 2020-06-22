e17228bf882e2f160325545f1ee0e7f097568296
danielvanc <email@danielvanc.com>
2020-06-22 19:31:43 +0100

    Add initial code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import marked from 'marked';
 
 const IndexPage = () => {
+  marked.setOptions({
+    gfm: true,
+    breaks: true,
+  });
+  const defaultPreview =
+    '# Header \n\n ## sub header \n\n [my link](https://danielvanc.com) \n\n  `inline code`  \n\n    code \n        block \n\n - list item \n\n>a blockquote \n\n **some bolded text**';
+  const [
+    previewText,
+    setPreviewText,
+  ] = React.useState(defaultPreview);
+  const handleChange = e => {
+    setPreviewText(
+      marked(e.currentTarget.value)
+    );
+  };
+
   return (
-    <h1>
-      Start from <em>Scratch</em>!
-    </h1>
+    <div>
+      <textarea
+        id="editor"
+        onKeyUp={handleChange}
+      >
+        {defaultPreview}
+      </textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(previewText),
+        }}
+      />
+    </div>
   );
 };
 
